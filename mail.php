17ab54575fcb64c8695eae68e83440253cab5b68
edsayrs <edsayrs@gmail.com>
2014-05-02 17:24:05 -0600

    adding jason functions to webform

--- mail.php
+++ mail.php
@@ -44,12 +44,7 @@ $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);  
-?><!-- place your own success html below -->
- 
-Thank you for contacting us. We will be in touch with you very soon.
 
- 
-<?php
 }
-die();
+echo json_encode(array('message'=>'Success'));
 ?>
